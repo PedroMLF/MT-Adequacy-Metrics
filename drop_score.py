8e3c31be6460709fb295efd858b97e0a1e73637e
PedroMLF <22480495+PedroMLF@users.noreply.github.com>
2018-08-13 17:36:37 +0000

    Added length penalty to the drop_score metric

--- drop_score.py
+++ drop_score.py
@@ -1,3 +1,4 @@
+import math
 import argparse
 
 ##########################################################################################
@@ -44,8 +45,7 @@ def list_of_indeces(path, index):
     return list_
 
 
-def calculate_score(src_ref_ixs, src_mt_ixs, src_words, stopwords, filter_stopwords):
-
+def calculate_score(src_ref_ixs, src_mt_ixs, lp, src_words, stopwords, filter_stopwords):
     """
     Prints the final score of dropped words according to alignments.
     
@@ -54,6 +54,7 @@ def calculate_score(src_ref_ixs, src_mt_ixs, src_words, stopwords, filter_stopwo
                          aligned with reference words
     - src_mt_ixs       : list of lists with the index of source words that
                          aligned with mt predicted words
+    - lp               : float with the length penalty value
     - src_words        : list of lists with the source words
     - stopwords        : list with the stop words for the source language
     - filter_stopwords : boolean that indicates whether stopwords are dropped or not
@@ -83,7 +84,8 @@ def calculate_score(src_ref_ixs, src_mt_ixs, src_words, stopwords, filter_stopwo
             tot += len(skipped_ixs)
             nr_src_ref_aligned_words += len(src_ref)
     
-    print("DSW: {:.2f}%".format(100*float(tot)/nr_src_ref_aligned_words)) 
+    print("LP: {:.2f} DSW: {:.2f}".format(lp,
+                                          lp*100*float(tot)/nr_src_ref_aligned_words))
 
 
 ##########################################################################################
@@ -99,7 +101,7 @@ def main():
                         help="If provided, excludes alignments relative to stopwords")
     parser.add_argument("--stopwords_path", type=str,
                         help="Path to the stopwords file")
-    parser.add_argument("--test_source", type=str,
+    parser.add_argument("--src_path", type=str,
                         help="Path to the test source language file")
 
     # Required arguments
@@ -108,11 +110,16 @@ def main():
                           help="Path to the source/reference alignments")
     required.add_argument("--src_mt_align", type=str, required=True,
                           help="Path to the source/prediction alignments")
+    required.add_argument("--ref_path", type=str, required=True,
+                          help="Path to the reference translation")
+    required.add_argument("--cnd_path", type=str, required=True,
+                          help="Path to the candidate translation")
+    
 
     args = parser.parse_args()
 
-    if (args.filter_stopwords or args.stopwords_path or args.test_source):
-        if not (args.filter_stopwords and args.stopwords_path and args.test_source):
+    if (args.filter_stopwords or args.stopwords_path or args.src_path):
+        if not (args.filter_stopwords and args.stopwords_path and args.src_path):
             print("The three optional arguments are dependent of each other")
             print("Make sure you provide all of them when calling the script")
             exit()
@@ -120,13 +127,27 @@ def main():
     SRC_REF_PATH = args.src_ref_align
     SRC_MT_PATH = args.src_mt_align
     STOPWORDS_PATH = args.stopwords_path
-    SOURCE_PATH = args.test_source
-    
+    SOURCE_PATH = args.src_path
+    REF_PATH = args.ref_path
+    CND_PATH = args.cnd_path
+
     # Source words that were aligned with some reference word
     src_ref_ixs_aligned = list_of_indeces(SRC_REF_PATH, 0)
 
     # Source words that were aligned with some MT predicted word
     src_mt_ixs_aligned = list_of_indeces(SRC_MT_PATH, 0)
+
+    # Calculate the length penalty
+    ref_sentence_list = [line.split() for line in open(REF_PATH, 'r')]
+    cnd_sentence_list = [line.split() for line in open(CND_PATH, 'r')] 
+
+    ref_length = sum([len(x) for x in ref_sentence_list])
+    cnd_length = sum([len(x) for x in cnd_sentence_list]) 
+
+    if cnd_length > ref_length:
+        lp = 1/(math.exp(1-(cnd_length/ref_length)))
+    else:
+        lp = 1
 
     # Print the final score    
     if args.filter_stopwords:
@@ -137,13 +158,15 @@ def main():
         # Create the list with the desired stopwords
         stopwords = [line.strip("\n") for line in open(STOPWORDS_PATH, 'r')]
 
-        calculate_score(src_ref_ixs_aligned, src_mt_ixs_aligned, src_words, stopwords, 
-                                                                args.filter_stopwords) 
+        # Calculate the final score
+        calculate_score(src_ref_ixs_aligned, src_mt_ixs_aligned, lp,
+                        src_words, stopwords, args.filter_stopwords) 
 
     else:
+        # Calculate the final score
         empty_src_words = [list() for entry in src_ref_ixs_aligned]
-        calculate_score(src_ref_ixs_aligned, src_mt_ixs_aligned, empty_src_words, list(),
-                                                                   args.filter_stopwords)
+        calculate_score(src_ref_ixs_aligned, src_mt_ixs_aligned, lp,
+                        empty_src_words, list(), args.filter_stopwords)
 
 
 if __name__ == "__main__":
