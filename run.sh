e91f40654807326e0242c1ef85d1458fad7f080f
PedroMLF <22480495+PedroMLF@users.noreply.github.com>
2018-10-17 20:24:37 +0000

    Fixed the paths in bash scripts

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ if [ "$#" != "4" ]
 then
     echo "Illegal number of parameters"
     echo "Usage:"
-    echo "./run_coverage_metrics.sh merged_prediction test_target src_ref_align src_mt_align"
+    echo "./run_metrics.sh merged_prediction test_target src_ref_align src_mt_align"
     echo "Example:"
-    echo "./run_coverage_metrics.sh <PATH>/merged.prediction.en <PATH>/newstest.en <PATH>/src_ref.align <PATH>/src_mt.align"
+    echo "./run_metrics.sh <PATH>/merged.prediction.en <PATH>/newstest.en <PATH>/src_ref.align <PATH>/src_mt.align"
     exit
 fi
  
@@ -18,8 +18,7 @@ echo $(basename $PRED)
 
 python3 rep_score.py \
         -r ${REF} \
-        -p ${PRED} \
-        --normalize
+        -p ${PRED}
 
 python3 drop_score.py \
         --src_ref_align ${SRC_REF_ALIGN} \
--- run_several.sh
+++ run_several.sh
@@ -1,20 +1,20 @@
 ALIGNER=/home/ubuntu/fast_align/build
 
-SRC=$1
-TGT=$2
-TEST_SRC=$3
-TEST_TGT=$4
-ALIGNER_DATA=$5
-MT_DATA=$6
+SRC=$1                # source language prefix
+TGT=$2                # target language prefix
+TEST_SRC=$3           # test file for source language
+TEST_TGT=$4           # test file for target language
+ALIGNER_DATA=$5       # folder with the aligner files
+MT_DATA=$6            # folder with the obtained translations
 LP=${SRC}-${TGT}
 
 if [ "$#" != "6" ]
 then
     echo "Illegal number of parameters"
     echo "Usage:"
-    echo "./run_all_coverage_metrics_several.sh source_language target_language test_source test_target aligner_data_path mt_predictions_path"
+    echo "./run_several.sh source_language target_language test_source test_target aligner_data_path mt_predictions_path"
     echo "Example:"
-    echo "./run_all_coverage_metrics_several.sh ro en <PATH>/newstest.ro <PATH>/newstest.en <AlignerDataPath> <MtPredictionsPath>"
+    echo "./run_several.sh ro en <PATH>/newstest.ro <PATH>/newstest.en <AlignerDataPath> <MtPredictionsPath>"
     exit
 fi
 
@@ -25,8 +25,7 @@ do
 
     python3 rep_score.py \
             -r ${TEST_TGT} \
-            -p ${file} \
-            --normalize
+            -p ${file} 
 
     paste -d '\t' \
           ${TEST_SRC} \
